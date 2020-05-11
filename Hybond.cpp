d400a4597acf0ec2508430c600dbca3c8bbbe6a0
Troy Van Tiel <troyvantiel@gmail.com>
2020-05-12 11:53:03 +1200

    Add the rest of the project files

---
+++ Hybond.cpp
@@ -0,0 +1,51 @@
+#include <Python.h>
+#include <errno.h>
+#include <limits.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <Pyhelp.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <istream>
+#include <vector>
+
+
+
+using namespace std;
+
+int main()
+{
+	//Py_SetProgramName(argv[0]);
+	//Py_SetPythonHome("C:/usr/lib/python3.6");
+
+	//cout << "Test" << endl;
+	Py_Initialize();
+	cout << "Python initialized: " << Py_IsInitialized() << endl;
+	PyRun_SimpleString("print ('Hello World')");
+
+	Py_Finalize();
+
+	/*
+	char filename[] = "test.py";
+	FILE* fp;
+
+	Py_Initialize();
+	fp = _Py_fopen(filename, "r");
+	PyRun_SimpleFile(fp,filename);
+
+	Py_Finalize();
+*/
+	cout << "This is just before the helper class code" << endl;
+	CPyInstance pyInstance;
+	PyRun_SimpleString("print('Hello World from embedded python!!!')");
+	printf("\nPress any key to exit...\n");
+	if(!_getch()) _getch();
+
+
+
+	return 0;
+} 
