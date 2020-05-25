f6db86bd1a9a117ab66d97b0af0dd3a45816efb9
Troy Van Tiel <troyvantiel@gmail.com>
2020-05-25 13:13:40 +1200

    final deletion of files

--- test.py
+++
@@ -1,7 +0,0 @@
-print('External Python program running...')
-print('Hello world from python file')
-
-def getInteger():
-	print('Python function getInteger() called')
-	c = 100 + 300/60
-	return long(c)
